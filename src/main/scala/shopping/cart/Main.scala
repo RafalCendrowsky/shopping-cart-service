65afb9b103b9de001f165fa036e96a1daee48c3e
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2023-05-03 14:20:25 +0200

    init

---
+++ src/main/scala/shopping/cart/Main.scala
@@ -0,0 +1,31 @@
+package shopping.cart
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.ActorSystem
+import akka.management.cluster.bootstrap.ClusterBootstrap
+import akka.management.scaladsl.AkkaManagement
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.util.control.NonFatal
+
+object Main {
+
+  val logger: Logger = LoggerFactory.getLogger("shopping.cart.Main")
+
+  def main(args: Array[String]): Unit = {
+    val system = ActorSystem[Nothing](Behaviors.empty, "ShoppingCartService")
+    try {
+      init(system)
+    } catch {
+      case NonFatal(e) =>
+        logger.error("Terminating due to initialization failure.", e)
+        system.terminate()
+    }
+  }
+
+  def init(system: ActorSystem[_]): Unit = {
+    AkkaManagement(system).start()
+    ClusterBootstrap(system).start()
+  }
+
+}
